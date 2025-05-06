ac6918019a23e11eaa20007ace3fc35dc544e687
Lucas <lucaslima15215@gmail.com>
2025-05-06 11:12:26 -0300

    :tada: Commit Inicial

---
+++ WebAppEmprestimos/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAppEmprestimos.Models;
+
+namespace WebAppEmprestimos.Data 
+
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)  
+        {
+        } 
+        
+        public DbSet<EmprestimosModel> Emprestimos { get; set; }
+
+    }
+}
---
+++ WebAppEmprestimos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppEmprestimos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
